45861747816bb6bbf37d419c1f57cb3dbb0ab0a3
huzaifak08 <hk7928042@gmail.com>
2022-07-21 12:05:49 +0500

    Firebase Auth for single button

---
+++ lib/home.dart
@@ -0,0 +1,75 @@
+import 'package:codecamp37/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            decoration: InputDecoration(
+              hintText: "Enter your Email",
+            ),
+          ),
+          TextField(
+            controller: _password,
+            decoration: InputDecoration(hintText: "Enter Your Passoward"),
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+          ),
+          TextButton(
+            onPressed: () async {
+              // Initialize Firebase
+              await Firebase.initializeApp(
+                  options: DefaultFirebaseOptions.currentPlatform);
+
+              // FirebaseAuth
+              final email = _email.text;
+              final password = _password.text;
+              final userCredential =
+                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                email: email,
+                password: password,
+              );
+              print(userCredential);
+            },
+            child: Text("Register"),
+          )
+        ],
+      ),
+    );
+  }
+}
